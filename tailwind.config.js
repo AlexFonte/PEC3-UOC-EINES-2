68627113ca2ef60e1b55d4a7f3e7df6fc82c5be9
Alex Fonte <aramosdf@gmail.com>
2025-06-03 23:04:21 +0200

    header done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,26 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "verde-texto": "#3a7044",
-        "beis-fondo": "#f8ffe5",
-        "verde-fondo": "#359c48",
-        "blanco-texto": "#ffffff",
-        // Añade aquí tus colores personalizados
+        "color-green": "#2e7d32",
+        "color-green-light": "#a5d6a7",
+        "color-veige": "#f1f8e9",
+        "color-white": "#ffffff",
+        "color-green-dark": "#3b6f6e",
+        "color-grey": "#616161",
+        "color-blue": "#0277bd",
+        "color-violet": "#5e35b1",
+        "color-orange": "#ff7043",
+        "bg-web": "color-scale-lightness($color_green, 95%)",
+        "accordion-button-active-bg": "color(display-p3 0.89 0.2 0.97 / 0.50)",
       },
       fontFamily: {
         titulo: ["Poppins", "sans-serif"],
         texto: ["Onest", "sans-serif"],
       },
     },
+    fontSize: {
+      '2xl': '2rem',
+    }
   },
   plugins: [],
 };
